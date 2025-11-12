af1fec25b3a2b82c6d7fed6523fe031078f4019d
Ivan Ren Villamora <villamora.ivanren.manguiat@gmail.com>
2025-11-12 21:18:40 +0800

    Improve login and sign-up responsiveness

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -75,79 +75,93 @@ const Login = () => {
   };
 
   return (
-    <div className="min-h-screen w-full flex flex-col sm:flex-row">
-      <SidebarContent className="hidden sm:flex sm:flex-1 sm:min-h-screen" />
-      <div className="w-full sm:flex-1 sm:min-h-screen flex flex-col items-center justify-center bg-[#FDFCFA] py-12 px-6 sm:px-10">
-        main
-        <form
-          className="w-full max-w-md sm:max-w-none lg:max-w-lg"
-          onSubmit={handleSubmit}
-        >
-          <div className="flex flex-col items-center text-center mb-10 space-y-2 sm:space-y-3">
-            <h1 className="font-poppins text-center text-h1 font-bold text-[#1C253C]">
-              Welcome!
-            </h1>
-            <p className="font-poppins text-[#1C253C] text-center text-paragraph">
-              Enter your credentials below to access your dashboard and saved
-              features.
-            </p>
+    <div className="min-h-dv w-full flex flex-col sm:flex-row">
+      <SidebarContent />
+      <div className="w-full h-dvh sm:flex-1 sm:min-h-screen relative bg-[#FDFCFA] px-6 sm:px-10">
+        <Link to="/">
+          <div className="sm:hidden py-4 flex gap-2 absolute top-0 left-4">
+            <img
+              src="src/assets/images/smartcane-logo-blue.png"
+              alt="Smart Cane Logo"
+              width={45}
+              className="object-contain"
+            />
+
+            <h1 className="font-gabriela text-4xl text-card-100">icane</h1>
           </div>
+        </Link>
 
-          {errors.general && (
-            <p className="font-poppins text-center text-[#CE4B34] mb-6">
-              {errors.general}
-            </p>
-          )}
+        <div className="h-screen flex flex-col items-center justify-center">
+          <form
+            className="w-full max-w-md sm:max-w-none lg:max-w-lg max-h-[70vh]"
+            onSubmit={handleSubmit}
+          >
+            <div className="flex flex-col items-center text-center mb-10 space-y-6 sm:space-y-3">
+              <h1 className="font-poppins text-center text-5xl sm:text-h1 font-bold text-[#1C253C]">
+                Welcome!
+              </h1>
+              <p className="font-poppins text-[#1C253C] text-center text-paragraph">
+                Enter your credentials below to access your dashboard and saved
+                features.
+              </p>
+            </div>
 
-          <div className="space-y-4">
-            <TextField
-              className="font-poppins"
-              label="Username"
-              placeholder="Enter your username..."
-              name="username"
-              value={formData.username}
-              onChange={handleChange}
-              error={errors.username}
-            />
+            {errors.general && (
+              <p className="font-poppins text-center text-[#CE4B34] mb-6">
+                {errors.general}
+              </p>
+            )}
 
-            <PasswordField
-              className="font-poppins relative"
-              label="Password"
-              placeholder="Enter your password..."
-              name="password"
-              required
-              type="password"
-              value={formData.password}
-              onChange={handleChange}
-              error={errors.password}
-              showErrorIcon={false}
-            />
+            <div className="space-y-4">
+              <TextField
+                className="font-poppins"
+                label="Username"
+                placeholder="Enter your username..."
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
+                error={errors.username}
+              />
 
-            <Link
-              to="/forgot-password"
-              className="font-poppins block text-left hover:underline text-[16px] underline mt-2"
-            >
-              Forgot password?
-            </Link>
+              <PasswordField
+                className="font-poppins relative"
+                label="Password"
+                placeholder="Enter your password..."
+                name="password"
+                required
+                type="password"
+                value={formData.password}
+                onChange={handleChange}
+                error={errors.password}
+                showErrorIcon={false}
+              />
 
-            <PrimaryButton
-              className="font-poppins w-full py-4 text-[18px] font-medium mt-6"
-              bgColor="bg-primary-100"
-              text="Sign In"
-              type="submit"
-            ></PrimaryButton>
-
-            <p className="font-poppins text-center text-[18px] mt-4">
-              Need An Account?{" "}
               <Link
-                to="/register"
-                className="font-poppins text-blue-500 hover:underline text-[18px]"
+                to="/forgot-password"
+                className="font-poppins block text-left hover:underline text-[16px] underline mt-2"
               >
-                Sign Up
+                Forgot password?
               </Link>
-            </p>
-          </div>
-        </form>
+
+              <PrimaryButton
+                className="font-poppins w-full py-4 text-[18px] font-medium mt-6"
+                bgColor="bg-primary-100"
+                text="Sign In"
+                type="submit"
+              />
+
+              <p className="font-poppins text-center text-[18px] mt-4">
+                Didn't have an account?{" "}
+                <Link
+                  to="/register"
+                  className="font-poppins text-blue-500 hover:underline text-[18px]"
+                >
+                  Sign Up
+                </Link>
+              </p>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
--- src/ui/components/SidebarContent.jsx
+++ src/ui/components/SidebarContent.jsx
@@ -1,27 +1,29 @@
-const SidebarContent = ({ className = "" }) => {
+const SidebarContent = () => {
   return (
-    <div
-      className={`w-full h-full min-h-screen sm:h-screen bg-primary-100 flex items-center justify-center flex-col relative ${className}`}
-    >
-      <img
-        src="src/assets/images/smartcane-logo.png"
-        alt="Sidebar Image"
-        className="opacity-5"
-      />
-      <div className="absolute top-1/2 -translate-y-1/2 flex items-center flex-col gap-y-24">
+    <>
+      <div
+        className={`hidden sm:flex sm:flex-1 sm:min-h-screen w-full h-full min-h-screen sm:h-screen bg-primary-100  items-center justify-center flex-col relative`}
+      >
         <img
           src="src/assets/images/smartcane-logo.png"
           alt="Sidebar Image"
-          width={290}
-          className=""
+          className="opacity-5"
         />
-        <h1 className="font-gabriela text-h1 text-[#FDFCFA]">icane</h1>
-        <p className="font-poppins text-paragraph text-[#FDFCFA] text-center max-w-nigga">
-          Bringing independence closer through a cane that’s more than just
-          support — it’s smart.
-        </p>
+        <div className="absolute top-1/2 -translate-y-1/2 flex items-center flex-col gap-y-24">
+          <img
+            src="src/assets/images/smartcane-logo.png"
+            alt="Sidebar Image"
+            width={290}
+            className=""
+          />
+          <h1 className="font-gabriela text-h1 text-[#FDFCFA]">icane</h1>
+          <p className="font-poppins text-paragraph text-[#FDFCFA] text-center max-w-nigga">
+            Bringing independence closer through a cane that’s more than just
+            support — it’s smart.
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
