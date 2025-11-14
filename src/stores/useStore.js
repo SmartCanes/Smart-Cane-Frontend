338678e808d90a3ce0215241171d020def41c1b8
Ivan Ren Villamora <villamora.ivanren.manguiat@gmail.com>
2025-11-14 15:50:12 +0800

    Persist storage

--- src/stores/useStore.js
+++ src/stores/useStore.js
@@ -1,9 +1,18 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useUserStore = create((set) => ({
-  userName: "",
-  isLoggedIn: false,
+export const useUserStore = create(
+  persist(
+    (set) => ({
+      userName: "",
+      isLoggedIn: false,
 
-  login: (username) => set(() => ({ userName: username, isLoggedIn: true })),
-  logout: () => set(() => ({ userName: "", isLoggedIn: false }))
-}));
+      login: (username) => set({ userName: username, isLoggedIn: true }),
+      logout: () => set({ userName: "", isLoggedIn: false })
+    }),
+    {
+      name: "user-session",
+      getStorage: () => sessionStorage
+    }
+  )
+);
