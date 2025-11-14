7ede24db32afe77be896f14ba6aeac1d203c423d
Ivan Ren Villamora <villamora.ivanren.manguiat@gmail.com>
2025-11-14 20:35:20 +0800

    Improve animation and chevron style

--- src/ui/components/FAQItem.jsx
+++ src/ui/components/FAQItem.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { Icon } from "@iconify/react";
+import chevronIcon from "@/assets/images/chevron.svg";
+import { motion, AnimatePresence } from "framer-motion";
 
 const FAQItem = ({ question, answer }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,14 +12,27 @@ const FAQItem = ({ question, answer }) => {
         className="w-full flex justify-between items-center cursor-pointer"
       >
         <span className="font-semibold text-card-100">{question}</span>
-        <Icon
-          icon={isOpen ? "akar-icons:chevron-up" : "akar-icons:chevron-down"}
-          className="text-xl"
-        />
+        <motion.div
+          animate={{ rotate: isOpen ? 180 : 0 }}
+          transition={{ duration: 0.25, ease: "easeInOut" }}
+        >
+          <img src={chevronIcon} alt="Chevron Icon" className="w-5 h-5" />
+        </motion.div>
       </button>
-      {isOpen && (
-        <p className="mt-2 text-[12px] text-gray-600 text-left">{answer}</p>
-      )}
+      <AnimatePresence initial={false}>
+        {isOpen && (
+          <motion.div
+            key="content"
+            initial={{ height: 0, opacity: 0 }}
+            animate={{ height: "auto", opacity: 1 }}
+            exit={{ height: 0, opacity: 0 }}
+            transition={{ duration: 0.25, ease: "easeInOut" }}
+            className="overflow-hidden"
+          >
+            <p className="mt-2 text-[12px] text-gray-600 text-left">{answer}</p>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
