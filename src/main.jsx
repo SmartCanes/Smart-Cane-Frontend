5d956452d2058432bb74d1088ba14e9a8082d73c
casixx <ryan.casipe78@gmail.com>
2025-11-18 21:40:39 +0800

    Guardian Acces, Manage Profile and Responsiveness

--- src/main.jsx
+++ src/main.jsx
@@ -16,6 +16,8 @@ import { ProtectedLayout, PublicLayout } from "./layout/LayoutRoute.jsx";
 // 👇 1. IMPORT MO YUNG WEATHERBOARD (Siguraduhin tama ang path kung nasa ui/components)
 import WeatherBoard from "./ui/components/WeatherBoard.jsx";
 import ScannerCamera from "./auth/Scanner";
+import GuardianAccess from "./ui/components/GuardianAccess.jsx";
+import ManageProfile from "./ui/components/ManageProfile.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -35,6 +37,8 @@ createRoot(document.getElementById("root")).render(
           <Route path="/dashboard" element={<Dashboard />} />
 
           {/* 👇 2. ADD MO ITONG ROUTE PARA SA WEATHER BOARD */}
+          <Route path="/guardian-access" element={<GuardianAccess />} />
+          <Route path="/manage-profile" element={<ManageProfile />} />
           <Route path="/weather-board" element={<WeatherBoard />} />
         </Route>
       </Routes>
