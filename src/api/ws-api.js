32307d11d291e46c22ae301bf2558485f0bdd83a
Ivan Ren Villamora <villamora.ivanren.manguiat@gmail.com>
2025-11-23 18:42:21 +0800

    Setup a socket connection

---
+++ src/api/ws-api.js
@@ -0,0 +1,42 @@
+import { io } from "socket.io-client";
+
+const WS_URL = import.meta.env.VITE_MIDDLEWARE_WS_URL || "http://localhost:4000";
+
+class SocketAPI {
+  constructor() {
+    this.socket = null;
+  }
+
+  connect() {
+    if (this.socket) return;
+
+    this.socket = io(WS_URL, {
+      transports: ["websocket"],
+      reconnectionAttempts: 5
+    });
+
+    this.socket.on("connect", () => {
+      console.log("Connected to Socket.IO server:", this.socket.id);
+    });
+
+    this.socket.on("disconnect", () => {
+      console.log("Disconnected from server");
+    });
+
+    this.socket.on("connect_error", (err) => {
+      console.error("Connection error:", err.message);
+    });
+  }
+
+  on(event, callback) {
+    if (!this.socket) this.connect();
+    this.socket.on(event, callback);
+  }
+
+  emit(event, payload) {
+    if (!this.socket) this.connect();
+    this.socket.emit(event, payload);
+  }
+}
+
+export const wsApi = new SocketAPI();
