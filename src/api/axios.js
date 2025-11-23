649cd9df0b456a2a280e9294183f6e06e833c99d
Ivan Ren Villamora <villamora.ivanren.manguiat@gmail.com>
2025-11-23 18:42:48 +0800

    Set an environment variable

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 // Set the backend URL
-const API_URL = "http://127.0.0.1:5000/api";
+const BACKEND_URL =
+  import.meta.env.VITE_BACKEND_API_URL || "http://127.0.0.1:5000/api";
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: BACKEND_URL,
   headers: {
     "Content-Type": "application/json"
   }
