f6f2f031fc4a8fa549802986dd607e7c76393d08
Ivan Ren Villamora <villamora.ivanren.manguiat@gmail.com>
2025-12-02 11:35:03 +0800

    Integrate validation modal and modal into one

--- src/ui/components/Modal.jsx
+++ src/ui/components/Modal.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { Icon } from "@iconify/react";
+import PrimaryButton from "./PrimaryButton";
 
 const MODAL_TYPES = {
   warning: {
@@ -29,16 +30,48 @@ const MODAL_TYPES = {
   }
 };
 
+const MODAL_VARIANTS = {
+  banner: {
+    positions: ["center", "top-right", "top-left", "top-center"],
+    size: "large",
+    showCloseButton: false,
+    hasHeaderImage: true,
+    headerColor: "#1C253C",
+    rounded: "3xl"
+  },
+  dialog: {
+    positions: ["center"],
+    size: "medium",
+    showCloseButton: true,
+    hasHeaderImage: false,
+    rounded: "2xl"
+  },
+  form: {
+    positions: ["center"],
+    size: "medium",
+    showCloseButton: true,
+    hasHeader: false,
+    rounded: "2xl",
+    hasInputs: true
+  }
+};
+
 export default function Modal({
   isOpen,
   onClose,
   title,
   message,
   modalType = "info",
+  variant = "dialog",
+  closeTimer = 5000,
+
+  actionText,
+  onAction,
+
   handleConfirm,
   handleCancel,
-  children, // custom content
-  footer // optional custom footer
+  children,
+  footer
 }) {
   useEffect(() => {
     const handleEscape = (e) => e.key === "Escape" && onClose();
@@ -46,12 +79,21 @@ export default function Modal({
     return () => document.removeEventListener("keydown", handleEscape);
   }, [onClose]);
 
-  if (!isOpen) return null;
+  useEffect(() => {
+    if (closeTimer === "none") return;
+    if (closeTimer && isOpen) {
+      const timer = setTimeout(() => {
+        onClose();
+      }, closeTimer);
+
+      return () => clearTimeout(timer);
+    }
+  }, [closeTimer, isOpen, onClose]);
 
   const type = MODAL_TYPES[modalType];
 
   return (
-    <AnimatePresence>
+    <AnimatePresence mode="wait">
       {isOpen && (
         <motion.div
           className="fixed inset-0 bg-black/50 backdrop-blur-sm flex justify-center items-center z-50 p-4"
@@ -60,61 +102,89 @@ export default function Modal({
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
         >
-          {/* Modal card */}
-          <motion.div
-            className="relative bg-white p-6 rounded-2xl shadow-xl w-full max-w-md font-poppins"
-            onClick={(e) => e.stopPropagation()}
-            initial={{ opacity: 0, scale: 0.9, y: 20 }}
-            animate={{ opacity: 1, scale: 1, y: 0 }}
-            exit={{ opacity: 0, scale: 0.9, y: 20 }}
-            transition={{ duration: 0.2 }}
-          >
-            {/* Close button */}
-            <button
-              onClick={onClose}
-              className="absolute top-4 right-4 text-gray-400 hover:text-gray-600 transition cursor-pointer"
+          {variant === "dialog" && (
+            <motion.div
+              className="relative bg-white p-6 rounded-2xl shadow-xl w-full max-w-md font-poppins"
+              onClick={(e) => e.stopPropagation()}
+              initial={{ opacity: 0, scale: 0.9, y: 20 }}
+              animate={{ opacity: 1, scale: 1, y: 0 }}
+              exit={{ opacity: 0, scale: 0.9, y: 20 }}
+              transition={{ duration: 0.2 }}
             >
-              <Icon icon="ph:x-bold" className="w-6 h-6" />
-            </button>
+              {MODAL_VARIANTS[variant].showCloseButton && (
+                <button
+                  onClick={onClose}
+                  className="absolute top-4 right-4 text-gray-400 hover:text-gray-600 transition cursor-pointer"
+                >
+                  <Icon icon="ph:x-bold" className="w-6 h-6" />
+                </button>
+              )}
 
-            {/* Header */}
-            <div className="flex items-start gap-3 mb-4">
-              <Icon
-                icon={type.icon}
-                className={`w-10 h-10 ${type.bg} p-1 rounded-full text-white`}
-              />
-              <div>
-                <p className="text-lg font-medium text-gray-900">{title}</p>
-                {!children && message && (
-                  <p className="text-sm text-gray-700 mt-1">{message}</p>
+              <div className="flex items-start gap-3 mb-4">
+                <Icon
+                  icon={type.icon}
+                  className={`w-10 h-10 ${type.bg} p-1 rounded-full text-white`}
+                />
+                <div>
+                  <p className="text-lg font-medium text-gray-900">{title}</p>
+                  {!children && message && (
+                    <p className="text-sm text-gray-700 mt-1">{message}</p>
+                  )}
+                </div>
+              </div>
+
+              {children && <div className="mb-4">{children}</div>}
+
+              {footer ? (
+                <div className="flex gap-3 mt-4">{footer}</div>
+              ) : handleConfirm || handleCancel ? (
+                <div className="flex gap-3 mt-4">
+                  <button
+                    onClick={handleCancel || onClose}
+                    className="flex-1 px-4 py-2 border border-gray-300 text-gray-900 rounded-lg hover:bg-gray-200 transition-colors cursor-pointer"
+                  >
+                    Cancel
+                  </button>
+
+                  <button
+                    onClick={handleConfirm}
+                    className={`flex-1 px-4 py-2 ${type.buttonColor} ${type.hover} text-white rounded-lg transition-colors font-bold cursor-pointer`}
+                  >
+                    Yes
+                  </button>
+                </div>
+              ) : null}
+            </motion.div>
+          )}
+
+          {variant === "banner" && (
+            <motion.div
+              className="bg-white rounded-2xl shadow-xl w-full max-w-md min-h-[350px] flex flex-col"
+              onClick={(e) => e.stopPropagation()}
+              initial={{ opacity: 0, scale: 0.9, y: 20 }}
+              animate={{ opacity: 1, scale: 1, y: 0 }}
+              exit={{ opacity: 0, scale: 0.9, y: 20 }}
+              transition={{ duration: 0.2 }}
+            >
+              <div className="bg-[#1C253C] rounded-t-2xl h-18"></div>
+              <div className="flex flex-col flex-1 gap-6 p-10 text-center rounded-b-2xl">
+                <h1 className="font-bold text-[#1C253C] text-3xl sm:text-4xl">
+                  {title}
+                </h1>
+                <div className="text-[#1C253C] leading-relaxed">
+                  {children || message}
+                </div>
+                {actionText && (
+                  <PrimaryButton
+                    className="w-full max-w-md text-[15px] sm:text-[18px] font-medium mt-auto"
+                    bgColor="bg-primary-100"
+                    text={actionText}
+                    onClick={onAction}
+                  />
                 )}
               </div>
-            </div>
-
-            {/* Content (custom children) */}
-            {children && <div className="mb-4">{children}</div>}
-
-            {/* Footer buttons */}
-            {footer ? (
-              <div className="flex gap-3 mt-4">{footer}</div>
-            ) : handleConfirm || handleCancel ? (
-              <div className="flex gap-3 mt-4">
-                <button
-                  onClick={handleCancel || onClose}
-                  className="flex-1 px-4 py-2 border border-gray-300 text-gray-900 rounded-lg hover:bg-gray-200 transition-colors cursor-pointer"
-                >
-                  Cancel
-                </button>
-
-                <button
-                  onClick={handleConfirm}
-                  className={`flex-1 px-4 py-2 ${type.buttonColor} ${type.hover} text-white rounded-lg transition-colors font-bold cursor-pointer`}
-                >
-                  Yes
-                </button>
-              </div>
-            ) : null}
-          </motion.div>
+            </motion.div>
+          )}
         </motion.div>
       )}
     </AnimatePresence>
