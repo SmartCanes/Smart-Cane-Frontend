869ae3532a32b4c045b319bac1d7f7aa68b54c79
Ivan Ren Villamora <villamora.ivanren.manguiat@gmail.com>
2026-01-15 14:49:30 +0800

    Remove sleep

--- src/api/requestHandler.js
+++ src/api/requestHandler.js
@@ -1,8 +1,5 @@
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
 export const handleRequest = async (request) => {
   try {
-    await sleep(2000);
     const { data } = await request();
     return data;
   } catch (err) {
