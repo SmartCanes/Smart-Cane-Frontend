26fff48e27a871a648d3c1603c5b2e2fa433bf47
Ivan Ren Villamora <villamora.ivanren.manguiat@gmail.com>
2026-03-21 11:19:27 +0800

    Add get device route

--- src/api/backendService.js
+++ src/api/backendService.js
@@ -204,3 +204,12 @@ export const getLastLocation = (deviceSerial) =>
   handleRequest(() =>
     backendApi.get(`/device/last-location/${encodeURIComponent(deviceSerial)}`)
   );
+
+export const getDeviceRoute = (deviceId) =>
+  handleRequest(() => backendApi.get(`/device/${deviceId}/route`));
+
+export const saveDeviceRoute = (deviceId, payload) =>
+  handleRequest(() => backendApi.put(`/device/${deviceId}/route`, payload));
+
+export const updateDeviceRouteStatus = (deviceId, payload) =>
+  handleRequest(() => backendApi.patch(`/device/${deviceId}/route/status`, payload));
